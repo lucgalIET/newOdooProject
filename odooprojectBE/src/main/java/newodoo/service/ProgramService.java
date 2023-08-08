b4514654dbde9994803da85a3d15635bd52b4110
stefadb <stefadb2@gmail.com>
2023-08-08 16:02:01 +0200

    program service finito

--- odooprojectBE/src/main/java/newodoo/service/ProgramService.java
+++ odooprojectBE/src/main/java/newodoo/service/ProgramService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.beans.FeatureDescriptor;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Service
@@ -21,9 +22,10 @@ public class ProgramService {
     //trova un programma (R)
     //aggiorna un programma (U)
     //cancella un programma (D)
-    public ProgramEntity addProgram(ProgramEntity program){
+    public ProgramEntity saveProgram(ProgramEntity program){
         return programRepository.save(program);
     }
+    public List<ProgramEntity> getAllPrograms(){return programRepository.findAll();}
     public ProgramEntity findById(Long id){
         return programRepository.findById(id).orElseThrow();
     }
@@ -39,7 +41,7 @@ public class ProgramService {
                 .filter(propertyName -> src.getPropertyValue(propertyName) == null)
                 .toArray(String[]::new);
     }
-    public void deleteProject(Long id){
+    public void deleteProgram(Long id){
         ProgramEntity programEntity = programRepository.findById(id).orElseThrow();
         programEntity.setDeleted(true);
         programRepository.save(programEntity);
