d1290b5952a7a503503f1c7cbdc71af4a61b0afc
PeppeSorb <giuseppesorbello98.ct@gmail.com>
2023-08-08 12:02:36 +0200

    Add project

---
+++ odooprojectBE/src/main/java/com/newodoo/newodoo/NewodooApplication.java
@@ -0,0 +1,13 @@
+package com.newodoo.newodoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewodooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewodooApplication.class, args);
+	}
+
+}
---
+++ odooprojectBE/src/test/java/com/newodoo/newodoo/NewodooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newodoo.newodoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewodooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
