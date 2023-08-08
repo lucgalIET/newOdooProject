fc06c4d4a8d1c9bdfd013a001f1415e974a06ef5
stefadb <stefadb2@gmail.com>
2023-08-08 14:48:57 +0200

    di nuovo tolti getter setter inutili

--- odooprojectBE/src/main/java/newodoo/entity/ProgramEntity.java
+++ odooprojectBE/src/main/java/newodoo/entity/ProgramEntity.java
@@ -2,67 +2,32 @@ package newodoo.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name="program")
-public class ProgramEntity{
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class ProgramEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private Long id;
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
-    @Column(name="program_manager")
+    @Column(name = "program_manager")
     private String programManager;
-    @Column(name="id_coo")
+    @Column(name = "id_coo")
     private Long idCoo;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idProgram") // Attributo nella classe Project che fa riferimento a program
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idProgram")
+    // Attributo nella classe Project che fa riferimento a program
     @JsonBackReference
     private List<ProjectEntity> projects;
-
-    // Costruttore vuoto
-    public ProgramEntity() {
-    }
-
-    // Costruttore con parametri
-    public ProgramEntity(String name, String programManager, Long idCoo) {
-        this.name = name;
-        this.programManager = programManager;
-        this.idCoo = idCoo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getProgramManager() {
-        return programManager;
-    }
-
-    public void setProgramManager(String programManager) {
-        this.programManager = programManager;
-    }
-
-    public Long getIdCoo() {
-        return idCoo;
-    }
-
-    public void setIdCoo(Long idCoo) {
-        this.idCoo = idCoo;
-    }
 }
