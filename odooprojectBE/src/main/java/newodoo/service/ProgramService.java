42b433d93147fe210edf3e21e654b4d71fa1f23e
stefadb <stefadb2@gmail.com>
2023-08-08 15:55:04 +0200

    Update ProgramService.java

--- odooprojectBE/src/main/java/newodoo/service/ProgramService.java
+++ odooprojectBE/src/main/java/newodoo/service/ProgramService.java
@@ -1,12 +1,47 @@
 package newodoo.service;
 
+import newodoo.Repository.ProgramRepository;
+import newodoo.Repository.ProjectRepository;
+import newodoo.entity.ProgramEntity;
+import newodoo.entity.ProjectEntity;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.beans.FeatureDescriptor;
+import java.util.stream.Stream;
 
 @Service
 public class ProgramService {
+    @Autowired
+    private ProgramRepository programRepository;
     //aggiungi un programma (C)
     //trova un programma (R)
     //aggiorna un programma (U)
     //cancella un programma (D)
-    public
+    public ProgramEntity addProgram(ProgramEntity program){
+        return programRepository.save(program);
+    }
+    public ProgramEntity findById(Long id){
+        return programRepository.findById(id).orElseThrow();
+    }
+    public ProgramEntity updateProgram(Long id, ProgramEntity programEntityUpdate){
+        ProgramEntity programEntity = findById(id);
+        BeanUtils.copyProperties(programEntityUpdate, programEntity, getNullPropertyNames(programEntityUpdate));
+        return programRepository.save(programEntity);
+    }
+    private String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        return Stream.of(src.getPropertyDescriptors())
+                .map(FeatureDescriptor::getName)
+                .filter(propertyName -> src.getPropertyValue(propertyName) == null)
+                .toArray(String[]::new);
+    }
+    public void deleteProject(Long id){
+        ProgramEntity programEntity = programRepository.findById(id).orElseThrow();
+        programEntity.setDeleted(true);
+        programRepository.save(programEntity);
+    }
 }
