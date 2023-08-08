f15d23e198d7f672362395bc7648c3b89547880c
stefadb <stefadb2@gmail.com>
2023-08-08 15:44:07 +0200

    update

---
+++ odooprojectBE/src/main/java/newodoo/service/ProgramService.java
@@ -0,0 +1,12 @@
+package newodoo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProgramService {
+    //aggiungi un programma (C)
+    //trova un programma (R)
+    //aggiorna un programma (U)
+    //cancella un programma (D)
+    public
+}
