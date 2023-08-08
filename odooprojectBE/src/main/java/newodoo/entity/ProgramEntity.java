c4fbacddd6b44127e94bde8d63f6dcc5bde6794f
stefadb <stefadb2@gmail.com>
2023-08-08 14:08:30 +0200

    Nuove classi aggiunte

---
+++ odooprojectBE/src/main/java/newodoo/entity/ProgramEntity.java
@@ -0,0 +1,59 @@
+package newodoo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ProgramEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String programManager;
+    private Long idCoo;
+
+    // Costruttore vuoto
+    public ProgramEntity() {
+    }
+
+    // Costruttore con parametri
+    public ProgramEntity(String name, String programManager, Long idCoo) {
+        this.name = name;
+        this.programManager = programManager;
+        this.idCoo = idCoo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProgramManager() {
+        return programManager;
+    }
+
+    public void setProgramManager(String programManager) {
+        this.programManager = programManager;
+    }
+
+    public Long getIdCoo() {
+        return idCoo;
+    }
+
+    public void setIdCoo(Long idCoo) {
+        this.idCoo = idCoo;
+    }
+}
