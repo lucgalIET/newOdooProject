68df9805839b38938b1bebf93416521bde8b0bd1
jackJack28 <139848078+jackJack28@users.noreply.github.com>
2023-09-12 17:53:51 +0200

    new routes made
    New routes have been created and the component " prog-user " have been updated.

--- odooprojectFE/src/app/app-routing.module.ts
+++ odooprojectFE/src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { MieiProgettiComponent } from './componenti/miei-progetti/miei-progetti.component';
 import { AssignmentsComponent } from './componenti/assignments/assignments.component';
 import { MieAttivitaComponent } from './componenti/mie-attivita/mie-attivita.component';
+import { AssignmentsProgettoComponent } from './componenti/assignments-progetto/assignments-progetto.component';
+import { MieAttivitaImportaComponent } from './componenti/mie-attivita-importa/mie-attivita-importa.component';
+import { AssignProgUserComponent } from './componenti/assign-prog-user/assign-prog-user.component';
 
 const routes: Routes = [
   {path: "", component: MieiProgettiComponent},
   {path: "assignments", component: AssignmentsComponent},
+  {path: "user", component: AssignProgUserComponent},
+  {path: "progetto", component: AssignmentsProgettoComponent},
   {path: "mieattivita", component: MieAttivitaComponent},
+  {path: "importa", component: MieAttivitaImportaComponent},
 ];
 
 @NgModule({
